bd8e3b537f773b3c11f956141a105cfa90bb915e
Ashutosh Gupta <akg@iitb.ac.in>
2019-03-04 23:52:21 +0530

    sad

--- README.md
+++ README.md
@@ -12,9 +12,20 @@ For taking random attendance in the class
   - db needs to be initialized
   - data has to be imported
 
-# Some help
+# Some Django help
 https://docs.djangoproject.com/en/2.1/intro/tutorial02/
 https://www.youtube.com/watch?v=UmljXZIypDc&index=1&list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p
 
 # to create a app inside the project
 python manage.py startapp
+
+# 
+
+# importing students in the DB
+
+- Goto ASC webpage with the list of students with pictures and without CPIs
+- Save the page from the browser (tested only on Firefox) to ~/downloads folder
+- Run ./scripts/asc-import.csv
+- Now go to the following webpage
+
+    http://127.0.0.1:8000/import/
--- studenthome/views.py
+++ studenthome/views.py
@@ -1,35 +1,18 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
 from django.template import loader, RequestContext
 from django.http import HttpResponse
 from .models import StudentInfo,Call
+
 import csv
 import os
 import shutil
-
+import datetime
 import random
 
-def index(request):
-    student_list = StudentInfo.objects.order_by('rollno')
-    if len(student_list) == 0:
-        return HttpResponse("No students in the class!!")
-    else:
-        # choose a random student
-        student = random.choice( student_list )
-        if student.presentCount + student.absentCount > 1:
-            # retry two times if the student has at least two calls
-            r_count = 0
-            while random.random() > 0.3 and r_count < 2:
-                studentp = random.choice( student_list )
-                if studentp.presentCount + studentp.absentCount < 2:
-                    student = studentp
-                    break
-                r_count += 1
-        context = RequestContext(request)
-        context.push( {'student': student, 'getstatus' : True, } )
-        return render( request, 'studenthome/index.html', context.flatten() )
-
+#----------------------------------------------------------------------------
+# utils
 def find_never_called(student_list):
     for student in student_list:
         called_count = student.absentCount+student.presentCount
@@ -37,7 +20,65 @@ def find_never_called(student_list):
             return student
     return None
 
-def ata(request):
+def get_or_none(model, *args, **kwargs):
+    try:
+        return model.objects.get(*args, **kwargs)
+    except model.DoesNotExist:
+        return None
+
+def get_call_list( student ):
+    call_list = []
+    calls = student.calls
+    while calls != None:
+        call_list.append(calls)
+        calls = calls.prevCall
+    return call_list
+
+def is_called_today( student ):
+    if student.calls == None:
+        return False
+    last_call = student.calls.created_on
+    if last_call.date() == datetime.date.today():
+        return True
+
+def pick_a_student(student_list):
+    # do not accept a student that is alreay called today
+    already_called_today = True
+    today_retry = 0 
+    while already_called_today and today_retry < 5:
+        # choose a random student
+        student = random.choice( student_list )
+        if student.presentCount + student.absentCount > 1:
+            # retry one time if the student has at least two calls
+            r_count = 0
+            while random.random() > 0.3 and r_count < 1:
+                # retry with 0.7 probability
+                studentp = random.choice( student_list )
+                if studentp.presentCount + studentp.absentCount < 2:
+                    # retry student has less than two calls,switch student
+                    student = studentp
+                    break
+                r_count += 1
+        already_called_today = is_called_today( student )
+        today_retry  += 1
+    return student
+
+#----------------------------------------------------------------------------
+# VIEWS
+
+# view to see a random student
+def index(request):
+    student_list = StudentInfo.objects.order_by('rollno')
+    if len(student_list) == 0:
+        return HttpResponse("No students in the class!!")
+    else:
+        student = pick_a_student( student_list )
+        context = RequestContext(request)
+        context.push( {'student': student, 'getstatus' : True, } )
+        return render( request, 'studenthome/index.html', context.flatten() )
+
+# view to find a student that is never called
+def never(request):
     student_list = StudentInfo.objects.order_by('rollno')
     if len(student_list) == 0:
         return HttpResponse("No students in the class!!")
@@ -47,21 +88,25 @@ def ata(request):
         context.push( {'student': student, 'getstatus' : True, 'ata': True} )
         return render( request, 'studenthome/index.html', context.flatten() )
 
+# view to look or modify student data
 def status(request, rollno):
-    student = get_object_or_404(StudentInfo, pk=rollno)
+    # student = get_object_or_404(StudentInfo, pk=rollno)
+    student = get_or_none(StudentInfo, pk=rollno)
+    if student == None:
+        return HttpResponse( "No student for roll number : " + str(rollno) )        
     if request.POST:
         st = request.POST['status']
-        if st == 'Absent':
-            student.absentCount += 1
-        if st == 'Present':
-            student.presentCount += 1
         if st == 'Absent' or st == 'Present':
+            if st == 'Absent':
+                student.absentCount += 1
+            elif st == 'Present':
+                student.presentCount += 1
             student.calls = Call.objects.create(
                 rollno=student.rollno,
                 status=st,
                 prevCall=student.calls)
             student.save()
-        if st == 'Delete' or st == 'Flip Attendance':
+        elif st == 'Delete' or st == 'Flip Attendance':
             time = request.POST['event']
             c = student.calls
             i = 0
@@ -83,7 +128,6 @@ def status(request, rollno):
                         student.absentCount += 1
                         student.presentCount += -1
                     c.save()
-                    student.save()
                 elif st == 'Delete':
                     if c.status == 'Absent':
                         student.absentCount += -1
@@ -96,18 +140,25 @@ def status(request, rollno):
                         next_call.save()
                     c.prevCall = None
                     c.delete()
-                    student.save()
+                student.save()
             else:
                 return HttpResponse("no call found for time " + time)
-    call_list = []
-    calls = student.calls
-    while calls != None:
-        call_list.append(calls)
-        calls = calls.prevCall
-    context = RequestContext(request)
-    context.push( {'student': student, 'getstatus' : False, 'call_list' : call_list } )
-    return render( request, 'studenthome/index.html', context.flatten() )
+        if st == 'Remove student':
+            student_calls = get_call_list( student )
+            for c in reversed(student_calls):
+                c.delete()
+            student.calls = None
+            student.save()
+            student.delete()
+            return HttpResponse( "Student for roll number " + str(rollno) + " has been removed." )
+        return redirect( "/"+str(student.rollno)+"/" )
+    else:
+        call_list = get_call_list( student )
+        context = RequestContext(request)
+        context.push( {'student': student, 'getstatus' : False, 'call_list' : call_list } )
+        return render( request, 'studenthome/index.html', context.flatten() )
 
+# view for status for all the students
 def all_status(request):
     student_list = StudentInfo.objects.order_by('rollno')
     count_list = dict()
@@ -125,11 +176,7 @@ def all_status(request):
             count_list[called_count] = 1
         if called_count > max_called_count:
             max_called_count = called_count
-        call_list = []
-        student_calls = student.calls
-        while student_calls != None:
-            call_list.append(student_calls)
-            student_calls = student_calls.prevCall
+        call_list = get_call_list( student )
         print_calls[student.rollno] = call_list
     called_idxs = []
     called_counts = []    
@@ -144,6 +191,7 @@ def all_status(request):
     return render( request, 'studenthome/all.html', context.flatten() )
 
 
+# view for loading student list from ASC website
 def db_import(request):
     imported=''
     csv_file = os.path.expanduser('/tmp/output.csv')
@@ -163,7 +211,7 @@ def db_import(request):
                     shutil.copy('/tmp/'+row[3],'studenthome/images/'+row[3])
                     imported=imported + row[1]+"," + row[2]+","+row[3]+"<br>"
     except IOError as e:
-        return HttpResponse( "Couldn't open or write to file"+csv_file )
+        return HttpResponse( "Failed to open file "+csv_file + ".<br> Look into README for importing students!")
     deleted="To be deleted students:<br>"
     deleted= deleted+"(to avoid accedental deletion, user needs to do it manually. goto <a href=\"admin/\">admin</a>)<br>"
     student_list = StudentInfo.objects.order_by('rollno')
