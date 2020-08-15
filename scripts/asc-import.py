eea7e8f146036b95ec4f48d5bc9fa99e57df9937
ASHUTOSH GUPTA <akg@iitb.ac.in>
2020-08-16 04:16:47 +0530

    asdd

--- scripts/asc-import.py
+++ scripts/asc-import.py
@@ -28,15 +28,33 @@ except IOError as e:
     sys.exit(0)
 
 flat_tds = re.sub(r'td>[\s]*<td', r'td><td', input, flags=re.M)
+flat_tds = re.sub(r'>[\s]*<a', r'><a', flat_tds, flags=re.M)
+flat_tds = re.sub(r'>[\s]*<b>', r'><b>', flat_tds, flags=re.M)
+flat_tds = re.sub(r'b>[\s]*</a>[\s]*</td', r'b></a></td', flat_tds, flags=re.M)
+# print(flat_tds)
 
-p = re.compile(r'(\d?\d?\d)\.\*?</b></td><td align="center"><b> ([0-9A-Z]+)</b></td><td> ([a-zA-Z \.]*)</td><td align="center"> Credit </td><td><img src="CourseList_data/([a-zA-Z0-9_]+.jpeg)')
+#2018 asc pattern
+#p = re.compile(r'(\d?\d?\d)\.\*?</b></td><td align="center"><b> ([0-9A-Z]+)</b></td><td> ([a-zA-Z \.]*)</td><td align="center"> Credit </td><td><img src="CourseList_data/([a-zA-Z0-9_]+.jpeg)')
+
+# 2020 ASC pattern
+p = re.compile(r'(\d?\d?\d)\.\*?</b></td><td align="center"><a href="[^<>"]*"><b> ([0-9A-Z]+)</b></a></td><td> ([a-zA-Z \.]*)</td>(<td>[^><"]*</td>){4}<td align="center">[^><"]*</td><td>[^><"]*</td><td><img src="CourseList_data/([a-zA-Z0-9_]+.jpeg)')
+
 out = p.findall(flat_tds)
 os.chdir(jpeg_dir)
 for o in out:
-    student_image_file = dump_dir+o[1]+".jpeg"
-    shutil.copy(o[3], student_image_file)
-    print ( "File created : "+ student_image_file)
-    output.write("%s,%s,%s,%s\n" % (o[0],o[1],o[2],o[1]+".jpeg"))
+    # print(o)
+    index  = o[0]
+    rollno = o[1]
+    name   = o[2]
+    photo  = o[4]     # o[3] is junk    
+    if os.path.isfile( photo ):
+        student_image_file = rollno+".jpeg"
+        shutil.copy(photo, dump_dir+student_image_file)
+        print ( "File created : "+ dump_dir+student_image_file)
+    else:
+        print ( "Missing photo : "+ rollno )
+        student_image_file = "none"       
+    output.write("%s,%s,%s,%s\n" % (index,rollno,name,student_image_file))
 
 print ( "File created : "+ out_file)  
 print ( "Students processed : "+ str(len(out)))
